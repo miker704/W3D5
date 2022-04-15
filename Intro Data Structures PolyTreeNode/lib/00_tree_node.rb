4ebdd5ec2a27b8d3148a678da9dec9407ed5aeea
miker704 <miker704@gmail.com>
2022-04-15 16:58:48 -0400

    updated polytreenode bfs function

--- Intro Data Structures PolyTreeNode/lib/00_tree_node.rb
+++ Intro Data Structures PolyTreeNode/lib/00_tree_node.rb
@@ -1,44 +1,98 @@
+require 'byebug'
 class PolyTreeNode
 
-    attr_accessor :value,:parent,:children
+    attr_reader :value,:parent,:children
     def initialize(value)
         @value=value
         @parent = nil
         @children=[]
     end
 
-    def parent=(some_node)
-        return @parent=nil if some_node==nil
-        return if some_node.children.include?(self)
+
+    #  calls a child to assign its parent node
+    def parent=(parent_node)
+        # return @parent=nil if some_node==nil
+
+        
+        # end
+        if parent_node == nil
+            @parent.children.delete(self)
+            return @parent=nil
+        end
+
+        return if parent_node.children.include?(self)
+
         if @parent!=nil
             @parent.children.delete(self)
             @parent=nil
 
         end
 
-        @parent=some_node
-        some_node.children.push(self)
+        @parent=parent_node
+        parent_node.children.push(self)
     end
 
 
+# parent call node to be its child
     def add_child(node)
         node.parent=(self)
     end
 
-    def remove_child(node)
+    # calls the parent node to remove some child node
+    def remove_child(child_node)
         raise 'I have no children!' if self.children.empty?
-        raise "thats is not my child!" if !self.children.include?(node) 
-        node.parent=(nil)
+        raise "thats is not my child!" if !self.children.include?(child_node) 
+        child_node.parent=(nil)
 
     end
 
 
+# [0]==target 
+# self[0] [2,3] -> 2==4 [4,5] 4==
+
+# return result if result != nill 
+#  
+    # stack
+    
     def dfs(target)
-
+        # debugger
+        return self if self.value==target 
+     
+        self.children.each do |child_node|
+            result=child_node.dfs(target)
+            return result if result!=nil
+        end
+        
+        return nil
     end
 
+    # queue
     def bfs(target)
+        # queue=[self]
+        # queue.each do |node|
+        #     result=queue.shift()
+        #     return result if result == target
+        #     queue.push(node.children)
+        # end
+        queue=[self]
+        queue.each do |node|
+            # if(queue.first.value==target)
+            #     return queue.first
+            # else
+            #     result = queue.shift()
+                
+            #     queue.unshift(result.children)
+            # end
 
+            result = queue.shift()
+            if(result.value==target)
+                return result
+            else
+                 queue.unshift(result.children)
+            end
+
+        end
+        return nil
     end
 
 end
--- README.md
+++ README.md
@@ -2,4 +2,5 @@ W3D5 - Intro to DSA - PolyTreeNode
 
 - Authors App/acc 
 - Michael R : https://github.com/miker704
-- Duncan M : https://github.com/dmurchison
+ - Karen P : https://github.com/kpolanco-arch
+ 
