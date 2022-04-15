9d7373ecd8b337d69d3f0945a618f4b61c6e88fe
miker704 <miker704@gmail.com>
2022-04-15 13:03:16 -0400

    Implemented PolyTreeNode methods

--- Intro Data Structures PolyTreeNode/lib/00_tree_node.rb
+++ Intro Data Structures PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,32 @@
 class PolyTreeNode
+    attr_accessor :value,:parent,:children
+    def initialize(value)
+        @value=value
+        @parent = nil
+        @children=[]
+    end
 
+    def parent=(some_node)
+        return @parent=nil if some_node==nil
+        return if some_node.children.include?(self)
+        if @parent!=nil
+            @parent.children.delete(self)
+            @parent=nil
+
+        end
+
+        @parent=some_node
+        some_node.children.push(self)
+    end
+
+
+    def add_child(node)
+        node.parent=(self)
+    end
+
+    def remove_child(node)
+        raise "thats is not my child!" if !self.children.include?(node)
+        node.parent=(nil)
+
+    end
 end
